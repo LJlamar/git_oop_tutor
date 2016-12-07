30268a93d055ed20b94993b04412e24fc40881ce
Alex <ljamon@yandex.ru>
2016-12-07 17:22:44 +0300

    Наследование

---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine : Figure
+    {
+        public VerticalLine(int x, int yTop, int yBot, char sym) //конструктор
+        {
+            pList = new List<Point>();
+            for (int y = yTop; y <= yBot; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+
+        }
+    }
+}
+
